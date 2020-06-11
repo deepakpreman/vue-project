5e9a77cfbef2b8dbde48f99c636ce63d5048374a
deepak.m@altencalsoftlabs.com <deepak.m@altencalsoftlabs.com>
2020-06-11 17:38:15 +0530

    code

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <app-navigation></app-navigation>
+        <side-bar></side-bar>
+        <v-content transition="slide-x-transition">
+            <router-view></router-view>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import AppNavigation from '@/components/AppNavigation';
+import SideBar from '@/components/SideBar';
+
+export default {
+    name: 'App',
+    components: {
+        AppNavigation,
+        SideBar
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        recipes: [],
+        apiUrl: 'https://api.edamam.com/search'
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        }
+    },
+    actions: {
+        async getRecipes({ state, commit }, plan) {
+            try {
+                let response = await axios.get(`${state.apiUrl}`, {
+                    params: {
+                        q: plan,
+                        app_id: '69b9a22a',
+                        app_key: '1279a7af24e7f204f8456f3344aecc5a',
+                        from: 0,
+                        to: 9
+                    }
+                });
+                commit('setRecipes', response.data.hits);
+            } catch (error) {
+                commit('setRecipes', []);
+            }
+        }
+    },
+    modules: {}
+});
